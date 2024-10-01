29fad78e5d338ea07b3899fae487a7a960ddc8ef
Greeshma594 <greeshmagokuldas369@gmail.com>
2024-10-01 16:14:23 +0530

    Delete utils.java

--- utils.java
+++
@@ -1,9 +0,0 @@
-// File: src/main/java/com/example/Utils.java
-package com.example;
-
-public class Utils {
-    public static void printMessage(String message) {
-        System.out.println("Utils: " + message);
-    }
-}
-
