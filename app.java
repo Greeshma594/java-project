e672ccee466741ebdcd2876d5cd000d3486fac85
EC2 Default User <ec2-user@ip-172-31-41-109.ap-southeast-2.compute.internal>
2024-10-01 10:02:08 +0000

    java project files

---
+++ app.java
@@ -0,0 +1,10 @@
+// File: src/main/java/com/example/App.java
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins and Maven!");
+        Utils.printMessage("This is a sample Java application.");
+    }
+}
+
---
+++ utils.java
@@ -0,0 +1,9 @@
+// File: src/main/java/com/example/Utils.java
+package com.example;
+
+public class Utils {
+    public static void printMessage(String message) {
+        System.out.println("Utils: " + message);
+    }
+}
+
